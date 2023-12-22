1e46c88d16cb2e7f7609455d86c5ed8e86c9923d
andrew62192 <96405778+andrew62192@users.noreply.github.com>
2023-12-23 04:43:11 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    //"./src/input.css", // 這是你新增的 styles.css 文件
+    //"./dist/output.css",
+    //"./src/input.html",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
